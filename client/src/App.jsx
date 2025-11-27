1b15b8bdcebdd676440edb1c188a13d839468ec5
Miguel Gandia <jenkins@pipeline.local>
2025-11-27 10:20:47 +0100

    Adding Carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect, useState } from "react";
-import { fetchItems } from "./api/items";
-import ItemsList from "./components/ItemsList";
-import Header from "./components/Header";
+import { fetchItems } from "./api/items";;
 import Hero from "./components/Hero";
-import Footer from "./components/Footer";
-import RetroCard from "./components/RetroCard";
 import GameList from "./components/GameList";
 import ConsoleList from "./components/ConsoleList";
+import ConsoleCarousel from "./components/ConsoleCarousel";
+import GameCarousel from "./components/GameCarousel";
+
 
 
 
@@ -28,15 +27,22 @@ function App() {
     <>
       <Hero />
 
-      <section className="max-w-6xl mx-auto px-4 py-10">
+      {/* <section className="max-w-6xl mx-auto px-4 py-10">
         <h2 className="text-2xl font-bold text-purple-400 mb-4">Juegos</h2>
         <GameList items={items} />
+      </section> */}
+      <section className="max-w-6xl mx-auto px-4 py-10">
+        <GameCarousel items={items} />
       </section>
 
       <section className="max-w-6xl mx-auto px-4 py-10">
+        <ConsoleCarousel items={items} />
+      </section>
+
+      {/* <section className="max-w-6xl mx-auto px-4 py-10">
         <h2 className="text-2xl font-bold text-purple-400 mb-4">Consolas</h2>
         <ConsoleList items={items} />
-      </section>
+      </section> */}
 
       
     </>
---
+++ client/src/components/ConsoleCarousel.jsx
@@ -0,0 +1,31 @@
+import RetroCard from "./RetroCard";
+
+export default function ConsoleCarousel({ items }) {
+
+  const consoles = items
+    .filter(item => item.type === "console")
+    .slice(0, 10); 
+
+  if (!consoles.length) {
+    return <p className="text-center text-gray-400">No hay consolas disponibles.</p>;
+  }
+
+  return (
+    <div className="relative w-full">
+      
+      <h2 className="text-2xl font-bold text-purple-400 mb-4">Consolas destacadas</h2>
+
+      <div className="flex gap-4 overflow-x-auto pb-4 snap-x snap-mandatory console-carousel-scrollbar">
+        {consoles.map((consoleItem) => (
+          <div
+            key={consoleItem._id}
+            className="snap-start min-w-[250px] max-w-[250px] flex-shrink-0"
+          >
+            <RetroCard item={consoleItem} />
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
